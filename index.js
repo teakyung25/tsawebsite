bef549c9802ba0da0a7f05c090bb0cac06103ac4
teakyung25 <31352970+teakyung25@users.noreply.github.com>
2018-05-13 15:18:33 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+  /*Turn on and off audio*/
+   var clicked = true;
+    $(".line-c").click( function() {
+  		 if (clicked) {
+    	 $(".line").addClass("stop");
+   		 clicked = false;
+         console.log("clicked true");
+          $("#backgroundaudio").trigger("pause");
+    } else {
+   		 $(".line").removeClass("stop");
+   		 clicked = true;
+         console.log("clicked false");
+         $("#backgroundaudio").trigger("play");
+  		}
+	});
+    
+ });
